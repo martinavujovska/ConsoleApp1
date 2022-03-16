db43d3099fcae75e24de0e90a7ea6c03301abbfd
Acer <Acer@DESKTOP-NE8MC2B>
2022-03-16 19:00:29 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            //Find the sum of the even numbers
+            int[] inputIntegers = new int[6];
+            int sumNumbers = 0;
+
+
+            for (int i = 0; i < inputIntegers.Length; i++)
+            {
+                Console.WriteLine("Input 6 integers:" +  (i + 1));
+                inputIntegers[i] = int.Parse(Console.ReadLine());
+                if (inputIntegers[i] % 2 == 0)
+                {
+                    sumNumbers += inputIntegers[i];
+                }
+            }
+            Console.WriteLine("Sum of even numbers is:" + sumNumbers);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = { "Martina", "Jovana", "Bojan", "Irena", "Elena" };
+            string[] studentsG2 = { "John", "Bob", "James", "Mark", "Simon" };
+
+            Console.WriteLine("Enter Student Group:");
+            int userInput = int.Parse(Console.ReadLine());
+            
+            for(int i = 0; i < 5; i++)
+            {
+                if (userInput == 1)
+                {
+                    Console.WriteLine("The students in G1 are:" + studentsG1[i]);
+                }
+                else if(userInput == 2)
+                {
+                    Console.WriteLine("The students in G2 are:" + studentsG2[i]);
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
